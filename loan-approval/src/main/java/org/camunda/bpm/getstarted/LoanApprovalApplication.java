1f711cb3bd6a9ce3f60eb0b8d1fb1c9a92725145
Uelinton Londero <ulondero@gmail.com>
2019-04-16 05:25:21 -0300

    create

---
+++ loan-approval/src/main/java/org/camunda/bpm/getstarted/LoanApprovalApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.getstarted;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication  {
+
+}
